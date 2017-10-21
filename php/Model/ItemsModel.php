a6bd33a0772c6769bb826781e759497e4842f7b1
Al-Saleh KEITA <alsaleh.keita@gmail.com>
2017-10-21 22:48:35 +0200

    modifs ItemsModel.php

--- php/Model/ItemsModel.php
+++ php/Model/ItemsModel.php
@@ -1,26 +1,18 @@
 <?php
-//require "crud.php";
-require "Model.php";
+require "Model.php";	// loads the PHP file
 
-//class Items extends USERS {
-//class Items extends CRUD {
 class ItemsModel extends Model {
-// Variables globales
-
-// Fonctions construct 
     function __construct() {
         parent::__construct();
     }
-	
-	/*
-	*	Add item to database
-	*	$item is array
-	*	return id last item
-	*/
 
-// Exercice 1 : Ecrire la fonction pour ajouter un item
-    public function addItem($item = array()) {
-		if(!isset($item['libelle'])) {  // nom de la variable et pas celui de la table ! 
+	/**
+	 * Add item to database
+	 * $item is array
+	 * return id last item
+	 */
+	public function addItem($item = array()) {
+		if(!isset($item['libelle'])) {   
 			return 0; 
 		}
 		elseif(!isset($item['description'])) {
@@ -41,124 +33,42 @@ class ItemsModel extends Model {
         elseif(!isset($item['categories_idcategories'])) {
 			return 0; 
 		}
-//        $item2 = $this -> insert($item, "items");
-	
-// Exercice 1 - Correction:
-		$item['code_item'] = $this -> randomByAlphNum("Items");
-		return $this -> insert($item, "items");
+
+		$item['code_item'] = $this->randomByAlphNum("Items");
+		return $this->insert($item, "items");
 	}
 
-// Exercice 2 : Ecrire la fonction pour modifier un item
-    public function updateItem($itemData=array()) {
-
-        // Exercice 2 - CORRECTION :       
+    public function updateItem($itemData) {
         if(!isset($itemData['iditems'])) {
             return 0;
         }
-
-        // if(!isset($itemData['libelle'])) {  
-		// 	return 0; 
-		// }
-		// elseif(!isset($itemData['description'])) {
-		// 	return 0; 
-		// }
-		// elseif(!isset($itemData['code_item'])) {
-		// 	return 0;
-        // }
-        // elseif(!isset($itemData['stocks'])) {
-		// 	return 0; 
-		// }
-		// elseif(!isset($itemData['availabity'])) {
-		// 	return 0; 
-		// }
-		// elseif(!isset($itemData['price'])) {
-		// 	return 0;
-        // }
-        // elseif(!isset($itemData['categories_idcategories'])) {
-		// 	return 0; 
-		// }
-        // $item3 = $this -> update($champs, $where, "items");
-    
 		$idItem = array("iditems" => $itemData['iditems']);
         unset($itemData['iditems']);
         $this->update($itemData, $idItem, "items");
-//        die("coucou!");
 	}
 		
-// Exercice 3 : Ecrire la fonction pour supprimer un item.
 	public function deleteItem($item = array()) {
 		if(!isset($item['iditems'])){
 			return 0;
 		}
-/* 		if(!isset($item['libelle'])) {
-			return 0; 
-		}
-		elseif(!isset($item['description'])) {
-			return 0; 
-		}
-		elseif(!isset($item['code_item'])) {
-			return 0;
-        }
-        elseif(!isset($item['stocks'])) {
-			return 0; 
-		}
-		elseif(!isset($item['availabity'])) {
-			return 0; 
-		}
-		elseif(!isset($item['price'])) {
-			return 0;
-        }
-        elseif(!isset($item['categories_idcategories'])) {
-			return 0; 
-		} */
-//		$item4 = $this -> delete($item, "items");
-		
-//		return $this -> delete($item, "items");
-		return $this -> select("note, commentaire, CONCAT(firstname, ' ', lastname) AS username", "reviews, clients", 
-		"items_iditems = $id AND clients_idclients = idclients");
-//		die("coucou!");
+		return $this->delete($item, "items");
     }
 	
-// Exercice 4 : Ecrire la fonction pour ajouter un item à une commande.
+
     public function addItemToOrder($orderId, $itemId,$qte) {
-/* 		if(!isset($item['orders_idorders'])) {  // nom de la variable et pas celui de la table ! 
-			return 0; 
-		}
-		elseif(!isset($item['items_iditems'])) {
-			return 0; 
-		}
-		elseif(!isset($item['qte'])) {
-			return 0;
-        }
-        elseif(!isset($item['price_final'])) {
-			return 0; 
-		} */
 		if(!is_int($orderId)) {
 			return 0;
 		}
 		elseif(!is_int($itemId)) {
 			return 0;
 		}
-//		$item5 = $this -> insert($item, "orders_has_items");
 
-//	Exercice 4 - Correction :
-//		die($price = $this -> select("price", "items", array("iditems" => $itemId)));
-		$price = $this -> select("price", "items", array("iditems" => $itemId));
-		$priceTotal = $price[0]["price"] * $qte;// english notation without thousands separator
+		$price = $this->select("price", "items", array("iditems" => $itemId));
+		$priceTotal = $price[0]["price"] * $qte;
 		$priceTotal = number_format($priceTotal, 2, '.', '');
 		settype($priceTotal, "float");
-		$idFavorite = $this -> insert(array("orders_idorders" => $orderId, "items_iditems" => $itemId, "qte" => $qte, 
-			"price_final" => $priceTotal), "orders_has_items");
-//		die("coucou!");
+		$idFavorite = $this->insert( array("orders_idorders"=>$orderId, "items_iditems"=>$itemId, "price_final"=>$priceTotal, "qte"=>$qte), "orders_has_items" );
     }
-
-// Exercice 5 : Ecrire la fonction pour supprimer un item à une commande.
-
-// Exercice 6 : Ecrire la fonction pour l'ajout / la suppression d'une catégorie item.
-
-// Exercice 7 : Ecrire la fonction pour afficher la liste des images (url) et de l'item.
-
-// Exercice 8 : Ecrire la fonction pour afficher la liste des types de l'item.
 
  	public function listenerItems($nbItem = 8) {
 		if(!is_int($nbItem)){
@@ -166,7 +76,6 @@ class ItemsModel extends Model {
 		}
 		return $this -> select("i.*, p.url", "`pictures` p, items i", "p.items_iditems = i.iditems 
 		GROUP BY i.iditems LIMIT " . $nbItem);
-//		die("coucou!");
 	}
 	
 	public function listenerPicturesItem($id) {
@@ -180,45 +89,32 @@ class ItemsModel extends Model {
 		if(!is_int($id)){
 			return -1;
 		}		
-		return $this -> select(
-//			"i.*, c.name as categorie, p.url, AVG(r.note) as reviewsMoyen", // SELECT //...	// on n'a plus besoin d'une partie de ce code car on utilise l'AJAX maintenant.
-			"i.*, c.name as categorie",
-//			"`items` i, categories c, pictures p, reviews r",	// FROM ...
-			"`items` i, categories c",	
-//			"i.categories_idcategories = c.idcategories AND i.iditems = p.items_iditems 
-//				AND r.items_iditems = i.iditems AND i.iditems = ". $id ." GROUP BY i.iditems"	// WHERE ... AND ... GROUP ... BY ...
-			"i.categories_idcategories = c.idcategories AND i.iditems = " . $id . " GROUP BY i.iditems"
+		return $this->select(
+			"i.*, c.name as categories", 
+			"`items` i, categories c", "i.categories_idcategories = c.idcategories AND i.iditems = " . $id . " GROUP BY i.iditems"
 		);
 	}
+
+	public function listenerItem2($id){
+		if(!is_int($id)){
+			return -1;
+		}
+		return $this->select("i.*, c.name as categories, p.url, AVG(r.note) as reviewsMoyen","items i, categories c, pictures p, reviews r", "i.`categories_idcategories` = c.idcategories AND i.iditems = p.items_iditems AND r.items_iditems = i.iditems AND i.iditems = ".$id." GROUP BY i.iditems");
+	}	
 
 	public function listenerReviewsItem($id) {
 		if(!is_int($id)) {
 			return -1;
 		}
-		return $this -> select("*", "reviews", array("items_iditems" => $id));
+
+		return $this->select("note, commentaire, CONCAT(firstname, ' ' ,lastname) as username", "reviews, clients", "items_iditems = $id AND clients_idclients = idclients");
 	}
 	
-	/* **************** CategorieModel ******************** */
+	/* **************** Categorie Model ******************** */
 	public function listenerCategories() {
-		return $this->select("name", "categories");	// on demande tous les names de la table categorie 
+		return $this->select("idcategories, name", "categories");	
 	}
 	
 }
-
-/*    $test = new ItemsModel();
-var_dump($test -> listenerItems());   */
-/* $test = new Items();
-$test -> addItemToOrder(10, 9, 12); */
-//sleep(10);
-
-/* 
---- Phase de tests ---
-$id = $test -> addItem(array('libelle' => "parfum", 'description' => "Beau parfum", 'code_item' => 242435, 'stocks' => 12, 'availabity' => 1, 
-    'price' => 50, 'categories_idcategories' => 1));
-$test -> updateItem(array("iditems"=>7, "libelle"=>'test un'));
-$test -> updateItem(array("iditems" => 9, "libelle" => "Café", "description" => "Arabica"));
-$test -> deleteItem(array("iditems" =>10));
-
-*/
 
 ?>
