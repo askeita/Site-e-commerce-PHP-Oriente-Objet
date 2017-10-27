b47540cc92157d13de43046e13fe47e3ebf6784f
Al-Saleh KEITA <alsaleh.keita@gmail.com>
2017-10-27 23:16:30 +0200

    modifs ApiController.php

--- php/Controller/ApiController.php
+++ php/Controller/ApiController.php
@@ -1,50 +1,39 @@
 <?php
 require "Controller.php";
-class ApiController extends Controller {
+class ApiController extends Controller {    // loads the PHP file
 
-	public function __construct() {
-		parent::__construct();
-	}
+    public function __construct() {
+            parent::__construct();
+    }
 	
     public function detailItem($id) {
-/* normalement on doit utiliser une méthode en GET, mais pour faire du REST API, on doit 
-               utiliser du POST */
-//        require "php/Model/ItemsModel.php";		 /* charger le fichier PHP. 
-//                Note : Pour les photos lancer "PhotosModel.php" */
-//        $dbItem = new ItemsModel();
-//        $picturesItem = $dbItem -> listenerPicturesItem($id);
- //       $reviewsItem =  $dbItem -> listenerReviewsItem($id);
 		
-		$picturesItem = $this->itemsModel->listenerPicturesItem($id);
+	$picturesItem = $this->itemsModel->listenerPicturesItem($id);
         $reviewsItem =  $this->itemsModel->listenerReviewsItem($id);
-		
-//        echo json_encode($pictureItem); // affiche tableau PHP en objet JavaScript
-        echo json_encode(array("pictures"=>$picturesItem, "reviews"=>$reviewsItem));
+        echo json_encode(array("pictures" => $picturesItem, "reviews" => $reviewsItem));
     }
 	
-	public function searchItems() {
-		$sql = "";
-		$search = false;
-		
-		if(isset($_POST["price"])){ // $_POST["price"] = valeur1 and valeur2
-			$sql .= "BETWEEN ".$_POST["price"]." AND ";
-			$search = true;
-		}
-		if(isset($_POST["categorie"])){	// $_POST["categorie"] = valeur1 and valeur2
-			$sql .= " categories_idcategories = ".$_POST["categorie"]." AND ";
-		}
-		
-		$sql = substr($sql, 0, -4);
-//		die($sql);
-		
-		if($search == false) // si aucun filtre n'est demandé
-			$sql = 1;
-		
-		$items = $this->itemsModel->select("*", "items", $sql);
-		
-		echo json_encode($items);
+    public function searchItems() {
+        $sql = "";
+        $search = false;
+
+        if(isset($_POST["price"])){ // $_POST["price"] = valeur1 and valeur2
+            $sql .= " price BETWEEN " . $_POST["price"] . " AND ";
+            $search = true;
+        }
+        if(isset($_POST["categorie"])){	// $_POST["categorie"] = valeur1 and valeur2
+            $sql .= " categories_idcategories = " . $_POST["categorie"] . " AND ";
+            $search = true;
+        }
+
+        if($search == false) // if no filter is asked
+            $sql = 1;
+        else
+            $sql .= " iditems = items_iditems GROUP BY iditems";
+            
+        $items = $this->itemsModel->select("i.*, p.url", "items i, pictures p", $sql);
+        echo json_encode($items);
     }
-	
 }
 
 ?>
