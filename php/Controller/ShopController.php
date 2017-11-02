fec5e1edb7d2597e40f45e0481dde58b0869500a
Al-Saleh KEITA <alsaleh.keita@gmail.com>
2017-11-02 22:10:47 +0100

    modifs ShopController.php

--- php/Controller/ShopController.php
+++ php/Controller/ShopController.php
@@ -6,27 +6,19 @@ class ShopController extends Controller {
 		parent::__construct();
 	}
 	
-//    public function itemsHome() {
     public function single($id) {
-//        require "php/Model/ItemsModel.php";		 // charger le fichier PHP
-//        $dbItem = new ItemsModel();
-//        $itemHome = $dbItem -> listenerItem($id);
-        $itemsHome = $this->itemsModel->listenerItem($id);		
-
-//        if(sizeof($itemsHome) != 1)
+		$itemHome = $this->itemsModel->listenerItem($id);
         if(sizeof($itemHome) != 1)			
-            header("Location: ".HOST.FOLDER."404");
+            header("Location: " . HOST . FOLDER . "404");
         else {
-//            $itemsHome = $dbItem->listenerItems();
 			$itemsHome = $this->itemsModel->listenerItems();		
 			require("shop-single.php");
-//            echo "<script>let idItem= " . $itemsHome[0]["iditems"] . ";let typePage = 1;</script>";
 			echo "<script>let idItem= " . $itemHome[0]["iditems"] . ";let typePage = 1;</script>";	// typePage permet de prendre en compte le nbre de requêtes Ajax
         }
     }
 	
 	public function shopListView() {
-		$itemsHome = $this->itemsModel->listenerItems();	// accès à la liste des 8 items
+		$itemsHome = $this->itemsModel->listenerItems(); // accès à la liste des 8 items
 		require("shop-list.php");
 		echo "<script>let typePage = 2;</script>";
 	}
