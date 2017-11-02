9f327c92d27e5a248b866f0708ebc9d1b9b46ecd
Al-Saleh KEITA <alsaleh.keita@gmail.com>
2017-11-02 21:59:33 +0100

    modifs HomeController.php

--- php/Controller/HomeController.php
+++ php/Controller/HomeController.php
@@ -5,21 +5,14 @@ class HomeController extends Controller {
 	public function __construct() {
 		parent::__construct();
 	}
-	
-	/*
-	*	Appeler au lancement de la route par défaut (la route /)
-	*/
-	
-//    public function itemsHome() {
-    public function home() {
-//		var_dump($this->categories); die();
-//        require "php/Model/ItemsModel.php";		 // charger le fichier PHP
-//        $dbItem = new ItemsModel();
-//        $itemsHome = $dbItem -> listenerItems();
-		$itemsHome = $this->itemsModel->listenerItems();	// Récupération des items de la BDD (les 8 premiers items).
-        include("home.php");	// Chargement de la view (page home.php)
-    }
 
+	/**
+	* Call to launch the default route
+	**/
+    public function home() {
+		$itemsHome = $this->itemsModel->listenerItems(); // Retrieval of the items from database ( by default the 8 first items)
+		include("home.php"); // loads view (page home.php)
+    }
 }
 
 ?>
